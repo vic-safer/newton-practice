8f92a3b897a7f211d22e5fd61e054b0ea41db6a5
vis-safer <victor.sana@berkeley.edu>
2025-08-20 10:34:58 -0700

    add newton.py second time

---
+++ newton.py
@@ -0,0 +1,15 @@
+def optimize(x0,f):
+    stop = 1e-2
+    h = 1e-4
+    f0 = f(x0)
+    df = (f(x0 + h) - f0) / h
+    d2f = (f(x0 + h) - 2*f0 + f(x0 - h)) / (h**2)
+    x1 = x0 - df / d2f
+    while abs(x1 - x0) > stop:
+        x0 = x1
+        f0 = f(x0)
+        df = (f(x0 + h) - f0) / h
+        d2f = (f(x0 + h) - 2*f0 + f(x0 - h)) / (h**2)
+        x1 = x0 - df / d2f
+    print("minimum found at:", x1)
+    return
